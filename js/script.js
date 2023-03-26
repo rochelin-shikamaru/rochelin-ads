4216f2114883ab634fe544435f7582eba8e35864
unknown <rochelinshikamaru@gmail.com>
2023-03-27 01:47:46 +0200

    udapte code

--- js/script.js
+++ js/script.js
@@ -18,3 +18,27 @@ setInterval(()=>{
     
    console.log(i);
 }, 5000);
+
+// animation à propos
+const js_scroll = document.querySelectorAll(".js-scroll");
+
+function elementInView(item, percentageScroll = 100) {
+    const distanceElement = item.getBoundingClientRect().top + 100;
+    return distanceElement <= window.innerHeight * (percentageScroll / 110);
+  }
+  
+  function displayScrollElement(item) {
+    item.classList.add("scrolled");
+  }
+  
+  function handleScrollAnimation() {
+    js_scroll.forEach((item) => {
+      if (elementInView(item, 100)) {
+        displayScrollElement(item);
+      }
+    });
+  }
+  
+  window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+  });
